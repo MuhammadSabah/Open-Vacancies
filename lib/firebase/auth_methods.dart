378b19f319449c654469eaa3376220707177c9a8
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2022-07-31 16:25:08 +0300

    Adding firebase auth

---
+++ lib/firebase/auth_methods.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final _auth = FirebaseAuth.instance;
+
+  Future<String?> signUpUser({
+    required String email,
+    required String password,
+  }) async {
+    String result = 'Error occurred';
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        result = 'The password provided is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        result = 'An account already exists for that email.';
+      } else if (e.code == 'invalid-email') {
+        result = 'Email is invalid.';
+      } else {
+        print(e);
+      }
+      return result;
+    } catch (e) {
+      print(e);
+      return e.toString();
+    }
+  }
+
+  Future<String?> logInUser({
+    required String email,
+    required String password,
+  }) async {
+    String result = 'Error occurred';
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'wrong-password') {
+        result = 'Password is incorrect.';
+      } else if (e.code == 'user-not-found') {
+        result = 'User is not registered.';
+      } else if (e.code == 'invalid-email') {
+        result = 'Invalid email.';
+      } else {
+        print(e);
+      }
+      return result;
+    } catch (e) {
+      print(e);
+      return e.toString();
+    }
+  }
+
+  void logOutUser() async {
+    await _auth.signOut();
+  }
+}
